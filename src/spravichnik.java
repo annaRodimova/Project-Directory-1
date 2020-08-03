d54938822cdb6aef9a3725bac4299ac29d854141
annaRodimova <antomashevich@mail.ru>
2020-08-03 19:49:40 +0300

    creature 1 persin

--- src/spravichnik.java
+++ src/spravichnik.java
@@ -1,2 +1,10 @@
+import java.util.*;
 public class spravichnik {
+    public static void main(String arga[]){
+        String[][] list = new String[1][2];
+        Scanner scan = new Scanner (System.in);
+        list[0][0]= scan.nextLine();
+        list[0][1]= scan.nextLine();
+        System.out.println(list[0][1]+ list[0][0]);
+    }
 }
