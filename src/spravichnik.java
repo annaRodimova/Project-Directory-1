8b0f25acad22707357a45380955aa09f91635e94
annaRodimova <antomashevich@mail.ru>
2020-08-03 20:49:09 +0300

    directory

--- src/spravichnik.java
+++ src/spravichnik.java
@@ -1,10 +1,32 @@
 import java.util.*;
 public class spravichnik {
-    public static void main(String arga[]){
-        String[][] list = new String[1][2];
+    public static String[][] Add(String name , String phone , String [][]list){
+        int size_column = list.length;
+        int size_rows = 2;
+        String [][] list_new = new String[size_column+1][size_rows];
+        for(int i = 0 ; i<size_column ; i++){
+            for(int j = 0 ; j<size_rows ; j++){
+                list_new[i][j] = list[i][j];
+            }
+        }
+        list_new[size_column][size_rows-2] = name;
+        list_new[size_column][size_rows-1] = phone;
+        return  list_new;
+    }
+    public static void display(String[][] list) {
+        for(int j = 0 ; j<list.length ; j++){
+            for (int i = 0 ; i<2 ; i++) {
+                System.out.print(list[j][i] + '\t');
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        String[][] list = new String[0][2];
         Scanner scan = new Scanner (System.in);
-        list[0][0]= scan.nextLine();
-        list[0][1]= scan.nextLine();
-        System.out.println(list[0][1]+ list[0][0]);
+        String new_name = scan.nextLine();
+        String new_phone = scan.nextLine();
+        String[][] new_list = Add(new_name, new_phone, list);
+        display(new_list);
     }
 }
