dc89cc88484640a249ff5ff0a4e073fb75b662f9
annaRodimova <antomashevich@mail.ru>
2020-08-07 20:03:02 +0300

    my new project ready

--- src/spravichnik.java
+++ src/spravichnik.java
@@ -1,23 +1,22 @@
 import java.util.*;
 public class spravichnik {
     public static String[][] Add(String name , String phone , String [][]list){
-        int size_column = list.length;
-        int size_rows = 2;
-        String [][] list_new = new String[size_column+1][size_rows];
+        int size_column = list.length; // считает не в индексах а в колл. элементов в транзитном списке
+        int size_rows = 2; // строки не меняются
+        String [][] list_new = new String[size_column+1][size_rows]; // здесь мы увеличиваем наш массив на 1 строку
         for(int i = 0 ; i<size_column ; i++){
             for(int j = 0 ; j<size_rows ; j++){
-                list_new[i][j] = list[i][j];
+                list_new[i][j] = list[i][j]; // в новый лист заполняем элементы старого
             }
         }
-        list_new[size_column][size_rows-2] = name;
-        list_new[size_column][size_rows-1] = phone;
+        list_new[size_column][0] = name; //даем обозначения нашим столбцам в последней строке в новом массиве
+        list_new[size_column][1] = phone;
         return  list_new;
     }
     public static void display(String[][] list) {
         for(int j = 0 ; j<list.length ; j++){
-            for (int i = 0 ; i<2 ; i++) {
-                System.out.print(list[j][i] + '\t');
-            }
+            System.out.print("имя " + list[j][0] + '\t');
+            System.out.print("телефон " + list[j][1] + '\t');
             System.out.println();
         }
     }
@@ -28,5 +27,11 @@ public class spravichnik {
         String new_phone = scan.nextLine();
         String[][] new_list = Add(new_name, new_phone, list);
         display(new_list);
+        while (scan.hasNext()){
+            new_name = scan.nextLine();
+            new_phone = scan.nextLine();
+            new_list = Add(new_name, new_phone, new_list);
+            display(new_list);
+        }
     }
 }
